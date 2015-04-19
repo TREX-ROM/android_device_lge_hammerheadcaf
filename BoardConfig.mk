4df3d0c20a2d6eb4d2d236e71b42e4142a8a0a15
LiquidDark <fisch890@gmail.com>
2015-04-19 09:41:18 +0200

    Hammerhead: More LIQUIFY

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,14 @@ BOARD_HAL_STATIC_LIBRARIES := libdumpstate.hammerhead
 # Define kernel config for inline building
 TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# LIQUIFY OPTIMIZATIONS V1
+# LIQUIFY := true
+
+# Changelog
+LIQUID_CHANGELOG := true
 
 # SELinux policies
 # QCOM
