92c396c6b429fe2db8308c544ff5b8be64ed0280
Sean Hoyt <seanhoyt963@gmail.com>
2015-05-27 18:06:31 -0500

    Liquify
    Remove Board and bootloader check

--- liquid.mk
+++ liquid.mk
@@ -1,20 +1,19 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerheadcaf/full_hammerhead.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerheadcaf
-PRODUCT_NAME := cm_hammerheadcaf
+PRODUCT_NAME := liquid_hammerheadcaf
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5 CAF
 PRODUCT_MANUFACTURER := LGE
