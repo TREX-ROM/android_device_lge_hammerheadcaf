bebe6365e5ba5fc90c6ed7e542ca952c9f39842a
LiquidDark <fisch890@gmail.com>
2015-06-04 19:11:27 +0200

    revert deconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/lge/hammerheadcaf
 BOARD_HAL_STATIC_LIBRARIES := libdumpstate.hammerhead
 
 # Define kernel config for inline building
-TARGET_KERNEL_CONFIG := liquiddark_hammerhead_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
